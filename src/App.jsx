459b82a86c0edf329c3ae30d67290218af65bcb1
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-23 20:30:43 +0530

    prop drilling solution: component composition

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -71,7 +72,16 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart}></Shop>
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product
+              {...product}
+              onAddToCart={() => handleAddItemToCart(product.id)}
+            />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
